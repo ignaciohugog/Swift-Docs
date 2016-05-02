e5197f8afcc5c9975705a0ef7ef465ed6e223542
Ignacio H. Gomez <ignaciohugog@gmail.com>
2016-05-02 20:31:37 -0300

    - Inheritance

---
+++ Official-Apple-Swift-Style-Guide/MyPlayground.playground/Pages/Inheritance.xcplaygroundpage/Contents.swift
@@ -0,0 +1,30 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+
+//subclass can overrid method and variables (@override)
+
+
+class Vehicle {
+	var currentSpeed = 0.0
+	var description: String {
+		return "traveling at \(currentSpeed) miles per hour"
+	}
+	func makeNoise() {
+		// do nothing - an arbitrary vehicle doesn't necessarily make a noise
+	}
+}
+
+class Car: Vehicle {
+	var gear = 1
+	override var description: String {
+		return super.description + " in gear \(gear)"
+	}
+}
+
+//Preventing Overrides
+//You can prevent a method, property, or subscript from being overridden by marking it as final.
