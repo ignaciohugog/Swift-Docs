d9b6444aa6de9b68ef73a08a5f4cd3aacd8f017f
ignaciohugog <ignaciohugog@gmail.com>
2024-08-06 10:50:06 +0200

    Functions

--- MyPlayground.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -1,40 +1,31 @@
-//: [Previous](@previous)
-
 import Foundation
 
 
-//: [Next](@next)
-
 /*
-Parameters
+    Parameters
 */
 
-//Default parameters
+//  Default parameters
 
-//if exist default parameter we can omit that parameter when calling the function.
-//place defaul parameters at the ende of the function in order to garantee same order and clarest
+//  if exist default parameter we can omit that parameter when calling the function.
+//  place default parameters at the end of the function in order to garantee same order and clarest
 func someFunction(parameterWithDefault: Int = 12) -> Int{
-	return parameterWithDefault + 1
+	parameterWithDefault + 1
 }
+
 someFunction()
 
-//Variadic parameters
+// Variadic parameters
 
 func arithmeticMean(numbers: Double...) -> Double {
-	var total: Double = 0
-	for number in numbers {
-		total += number
-	}
-	return total / Double(numbers.count)
+	Double(numbers.reduce(0, +)) / Double(numbers.count)
 }
-arithmeticMean(1, 2, 3, 4, 5)
-// returns 3.0, which is the arithmetic mean of these five numbers
-arithmeticMean(3, 8.25, 18.75)
-// returns 10.0, which is the arithmetic mean of these three numbers
+arithmeticMean(numbers: 1, 2, 3, 4, 5)
+arithmeticMean(numbers: 3, 8.25, 18.75)
 
-//In out parameters
+// In out parameters
 
-func swapTwoInts(inout a: Int, inout _ b: Int) {
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {
 	let temporaryA = a
 	a = b
 	b = temporaryA
@@ -43,32 +34,33 @@ var a = 1
 var b = 0
 swapTwoInts(&a, &b)
 
-//using function types
+// Function types
 func addTwoInts(a: Int, _ b: Int) -> Int {
-	return a + b
+	a + b
 }
 
 func multiplyTwoInts(a: Int, _ b: Int) -> Int {
-	return a * b
+	a * b
 }
 
 var mathFunction: (Int, Int) -> Int = addTwoInts
+
 print("Result: \(mathFunction(2, 3))")
 mathFunction = multiplyTwoInts
 
 func stepForward(input: Int) -> Int {
-	return input + 1
+	input + 1
 }
 
 func stepBackward(input: Int) -> Int {
-	return input - 1
+	input - 1
 }
-//return a function with type (Int) -> Int
+// Return a function with type (Int) -> Int
 func chooseStepFunction(backwards: Bool) -> (Int) -> Int {
 	return backwards ? stepBackward : stepForward
 }
 
-//neasted functions
+// Nested functions
 func chooseStepFunctionNeasted(backwards: Bool) -> (Int) -> Int {
 	func stepForward(input: Int) -> Int { return input + 1 }
 	func stepBackward(input: Int) -> Int { return input - 1 }
